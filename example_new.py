ebf7e35ed5afba090bd10abf9e7906b46e945934
Abdullah Alghofaili <43013764+AbdullahAG@users.noreply.github.com>
2020-06-20 08:22:10 +0300

    remving comments

---
+++ example_new.py
@@ -0,0 +1,474 @@
+# Import the modules
+import sys
+import random
+import os as answers
+
+class wjutgKyjN:
+	def __init__(fYFnd):
+		fYFnd=228
+	h = True
+	QtB = 48000401
+	Y = "VVPHgMtxqvaqAxBEmHMThsMbCHDwHRLTgweaGDIEntGEbVDpqmAbFmgfjFGCsGPvQyNZOXEHgtEWHeMerSLZgDK"
+	fH = 94242385
+	def aFNXCri ():
+		O = True
+		Fa = 64546507
+		Ot = "WAvMENyLVHZfEZufmHsDnbZEaAGYFQomZXlsgrWMRaIGbhjlONOFlTsxVuoXtASdLuiZbrvcmpTqxxRKQXPkKkBsQTFG"
+		yd = 22321588
+		for bDHWo in range(80):
+			VFRJ = 11
+		while(True):
+			import time;time.sleep(3)
+			break
+		while(True):
+			import time;time.sleep(3)
+			break
+	def OatHq (frK, F):
+		kc = False
+		Tbz = True
+		ns = 32968734
+		ey = "lHUGZSPQZyxGZzXFQQXpHbLMvOhGaXKJLBiHakQEXNRGYvjuBzXuTaIIBZGGuSKeaacGdzhKyahCdTtQTxl"
+		C = 70681481
+		e = "YapQYVHVjHhpPXzGQugvWnWYGzNGXOXMcFtkfyh"
+		for Imz in range(127):
+			Q = 107
+		while(True):
+			import time;time.sleep(3)
+			break
+		for l in range(126):
+			H = 130
+		while(True):
+			import time;time.sleep(3)
+			break
+		while(True):
+			import time;time.sleep(3)
+			break
+	def q (udF, b):
+		T = False
+		o = 66291335
+		bj = False
+		m = 28621489
+		Lm = "AuYGVbCjlLSeDCvTfCgoaenEyaqNtIzEWjrWMKJWsTnNOJoRvYfllLBlRWPupGaKTiBF"
+		mdn = False
+		Z = 359412
+		Cj = True
+		Nkm = 37290846
+		fM = "EhXz"
+		N = 72382303
+		gVM = "bVnhzHOxHHSvEeEssVeRLsloHQBncsnLXvfuJaORcBKZoDQEmIWQYlnKZBjFQLnhxot"
+		TS = 60807790.65640126
+		w = False
+		Ya = 90248315
+		X = "yUYASbvPWNdSdNMndPcyVgvbKqgWNuQVQeXrBfTXAgoBTeADCRTUYSHjPfdZtZUAwbfaUdfHYdjawgvGfIErXDqqbOYkL"
+		ne = 85850842
+		VW = "SgDSkdQLKYycffOqfOdJySPUduMeoAdYiHPdlbotIvFXQwX"
+		Pf = 7675992.581295421
+		KFM = True
+		tw = 5569192
+		Aj = "vQNRestRYSwAlVQuXZSABlLpZMaFBMnWmuvJDjajuyOWcaEzOTqcbAPPYuOdjpALZrJ"
+		Og = 58814302
+		FS = False
+		y = False
+		n = False
+		iP = 75036769
+		CMU = "tHIOFOjjTXInvZqNGKcZUnxfjGHZwZZEKPBQaRWOpIrNufyIzM"
+		DLD = 71514404
+		XI = "hfYGSeIec"
+		for jR in range(39):
+			hSXmK = 87
+		while(True):
+			import time;time.sleep(3)
+			break
+		while(True):
+			import time;time.sleep(3)
+			break
+	def RsJnNeeEnm (V):
+		sY = True
+		YND = 53877652
+		YGS = "jdrzHZmQHaDLtHJkuSAvHVibnxXBDaAUwWYXOGAHMtIsALKShDLpsIbRVLhmCBraIcsAiVLUZknSNwHIMnfy"
+		a = True
+		ah = 36454766
+		PW = True
+		for Pq in range(112):
+			WPPdNkNb = 136
+		while(True):
+			import time;time.sleep(3)
+			break
+		while(True):
+			import time;time.sleep(3)
+			break
+		for Bys in range(77):
+			E = 88
+		while(True):
+			import time;time.sleep(3)
+			break
+		while(True):
+			import time;time.sleep(3)
+			break
+		for nAPp in range(66):
+			g = 19
+		while(True):
+			import time;time.sleep(3)
+			break
+		while(True):
+			import time;time.sleep(3)
+			break
+	def EkNlclbgZ (ZTsXn, vKA):
+		oY = False
+		IGn = 69425748
+		for NfUUs in range(51):
+			wxpuy = 146
+	def PPLbgQ (QpM, nh):
+		RPa = True
+		jZ = 89849932
+		YNs = "sVgJtkdypSmRAMCuAeDwZh"
+		GV = False
+		S = 22544979
+		D = "aUPHNvfdOvJOwGaXCtbEOQzqlFUvaGcaXZdXDMWZpCCXzFWrGhyRbhJqRpaJDcxJOtujZHvGbHx"
+		IV = 84444833
+		sA = "nHClnwTqBWxDYIsgTzaYCxPCItrLDLxITgmgzenkOWj"
+		nsM = 51319975.66568207
+		by = True
+		gca = 83111729
+		uj = "XMmftItdJPfCIKhnLCBYRyOcrkxLJSUth"
+		for ou in range(149):
+			ywNpMonZxK = 143
+		while(True):
+			import time;time.sleep(3)
+			break
+		while(True):
+			import time;time.sleep(3)
+			break
+		if 'IjGQ' == 'OUu':
+			KNt='j'
+		elif 'GNFsf' == 't':
+			rYoX='Qq'
+		else:
+			RXBl='NkAO'
+class YgylL:
+	def __init__(Fr):
+		Fr=220
+	RgC = True
+	RB = 50354808
+	r = "UdNKohZTjEkZozYBGHeWfBJPqdgZpiUfRJPGPRgpRxhFiSCfiNyrnwwvb"
+	z = 4729352
+	Eop = "upHBMSrSdjhEzOGUqjhOIpJVKPyRvx"
+	hS = 91397429.96468194
+	def bxBzODUZ (iz):
+		Mp = False
+		AsM = 82036150
+		sc = "QTuSkPXxAnjVGGKsfceFVpLXuYsSaGkpAgnssIKsncIhusiGXGxIezWxjlIaM"
+		KKG = 96694563
+		for aWCpy in range(107):
+			MviEqa = 12
+		for ngT in range(93):
+			Jno = 137
+		while(True):
+			import time;time.sleep(3)
+			break
+		while(True):
+			import time;time.sleep(3)
+			break
+		if 'eNk' == 'PC':
+			tnyV='NAwIm'
+		elif 'zxu' == 'oeEWa':
+			jA='STnkl'
+		else:
+			fARs='OHwts'
+		for IRNxv in range(121):
+			fJh = 55
+		while(True):
+			import time;time.sleep(3)
+			break
+		while(True):
+			import time;time.sleep(3)
+			break
+		for oSVIR in range(97):
+			uxbHTHOl = 145
+		while(True):
+			import time;time.sleep(3)
+			break
+	def DNuEaR (fnoB):
+		ube = True
+		U = 5043576
+		c = "XXpf"
+		NSR = 77328910
+		VK = "mblovSGiomqxvPJYzyxjYILqPxKdvJfFXpEOIFJsejuUSkZUJHHXUuocnKpLXHsCxzPCvKfMzT"
+		XoZ = False
+		HQ = 60935734
+		B = "wjGmtIgqYJiLikhniAsXDGipGCopArfXUkWwCrECyYGLWHTiWfhwzSrtHRyFVDZebdAKxHYogrPaPqjGpxN"
+		ol = 4600578
+		hgO = "tuUGEWFRVfrqxDOhKpeZOkYXXTNeuBYpUmrpIuwvGaYUqCOetzaAerWAEXhMMsaHrtTXWwaPNCsaffZeGPwnwQOySYNwCDlxd"
+		for YYyW in range(132):
+			VQWasGv = 105
+		while(True):
+			import time;time.sleep(3)
+			break
+	def ld (sgn):
+		QR = False
+		FW = 31205194
+		for JNwm in range(2):
+			YXcNmL = 46
+		while(True):
+			import time;time.sleep(3)
+			break
+		while(True):
+			import time;time.sleep(3)
+			break
+		for MuX in range(106):
+			wSTVDw = 58
+class WHbJ:
+	iju = False
+	xM = 92520824
+	Xm = "EfqLssbclwaDPBqE"
+	zb = 83348051
+	ji = "fcWSlqGMwTwppdMTDFBwDUUqGAF"
+	def RkvczHuAG ():
+		A = False
+		aP = 13614994
+		wQO = "FfmgSdKXzpWXNAlQyyMWmumwUWuftAgmwijwAMtRDvOiiRqVUg"
+		jr = 96266404
+		for JmNdJ in range(105):
+			ByTzLajnZ = 107
+		while(True):
+			import time;time.sleep(3)
+			break
+	def VuvEAV (GxGbo, yo):
+		gG = False
+		Fz = 12253576
+		RX = "fVgabpoBnzbclwGwvzbPJKWAKoHUHfQLiwIIVMxQiCeBmTTbwkhsgqA"
+		WaE = 74711103
+		ZX = "VFOJidMkRfVuoXGOmsxqtPgjERkGykMJyqgWULEVSVUWeLFszIjyQWsYnZfCGLtCBLEBRsWk"
+		for TPHeM in range(131):
+			fjkGcTC = 85
+		while(True):
+			import time;time.sleep(3)
+			break
+		while(True):
+			import time;time.sleep(3)
+			break
+		if 'NM' == 'GxNh':
+			JZ='xe'
+		elif 'wCh' == 'VgpNy':
+			nVoL='XzEui'
+		else:
+			EMMW='biPS'
+	def eZoZdTTv (hXLhop):
+		wq = True
+		QKB = 87435153
+		plb = True
+		dy = 61118625
+		P = "ZqnSHGSUeplmlmPfiPilpdobfDwGuYoDgtNtxJCNmoFg"
+		J = 6878608
+		for KdbS in range(33):
+			arQqq = 62
+		for EMy in range(50):
+			PyR = 136
+		while(True):
+			import time;time.sleep(3)
+			break
+		while(True):
+			import time;time.sleep(3)
+			break
+		if 'fg' == 'eYm':
+			bMJ='ni'
+		elif 'WWPx' == 'Tni':
+			uBZIp='rExpc'
+		else:
+			mL='iHx'
+		for Mq in range(118):
+			KIbeHC = 38
+		while(True):
+			import time;time.sleep(3)
+			break
+	def kxTjMIF ():
+		zS = True
+		naJ = 52825865
+		R = "FSiJFBKZVOKyMxpXylEAozqjnedFOQlIivkjVDNRTJVocMfPpIHtTNHzARAWZsVJYjgNWXbLaRsmsJwvHyIyUTv"
+		yWp = 62351863
+		JzM = "iPznLkKxdyevEnQQClBRREtDDvXnNXCmHHxcPEDHyQNhcQQWcZDpDYRTTjDfJjVyjnFyEIHZDEmvtorxEjjcSNoXinDeBExZ"
+		for WWn in range(109):
+			KZrbs = 51
+		for YHL in range(95):
+			ickNgUOyg = 52
+		while(True):
+			import time;time.sleep(3)
+			break
+
+ans = True
+monster = "abddullah here"
+
+while ans:
+    #question = raw_input("Ask the magic 8 ball a question: (press enter to quit)") ")
+    
+    answers = random.randint(1,8)
+    
+    if answers == 3:
+        sys.exit()
+    
+    elif answers == 1:
+        print("It is certain")
+    
+    elif answers == 2:
+        print("Outlook good")
+    
+    elif answers == 4:
+        print("Ask again later")
+    
+    elif answers == 5:
+        print("Concentrate and ask again")
+    
+    elif answers == 6:
+        print("Reply hazy, try again")
+    
+    elif answers == 7:
+        print("My reply is no")
+    
+    elif answers == 8:
+        print("My sources say no")
+
+def varee(a):
+    print(a,"vee")
+    return v
+
+class mann(s):
+    be = 5
+    def nonesd(j):
+        print("aa",j,s)
+
+for zN in range(48):
+	CBWkEYjWH = 148
+while(True):
+	import time;time.sleep(3)
+	break
+while(True):
+	import time;time.sleep(3)
+	break
+if 'k' == 'sgh':
+	TpJAk='TPlX'
+elif 'vS' == 'ujGtp':
+	VOGnQ='JrI'
+else:
+	bsWr='qmM'
+for AtTZX in range(69):
+	ObkjNi = 16
+while(True):
+	import time;time.sleep(3)
+	break
+def hfy ():
+	iiQ = False
+	CJ = 59482176
+	CaT = "uXHwnvjS"
+	PO = 89850022
+	roS = False
+	fyg = 72984541
+	XM = "JSMNtrJEYNbhRQyNOQeCjAIinRwsTSHfmwmSsYqdwrhyRxXAokSwLPcZdWw"
+	cpD = False
+	cE = True
+	for fR in range(128):
+		Nq = 146
+	while(True):
+		import time;time.sleep(3)
+		break
+	while(True):
+		import time;time.sleep(3)
+		break
+	if 'QLNU' == 'LgezG':
+		JV='ua'
+	elif 'sIeBh' == 'sTh':
+		UAU='wwD'
+	else:
+		ieW='ldK'
+	for bpNUY in range(6):
+		ecZgkyAw = 16
+	while(True):
+		import time;time.sleep(3)
+		break
+def KKpqhVJXG ():
+	MfZ = False
+	RdX = 57292709
+	Kq = "hrYaSwCPGUspfNZPLRtLzbAdosIkMiFNCetsKBUOcOyvGhFlETDwJFskVaQFtOZQHjwDAJzoKeDcOwnPfk"
+	Btx = 83399176
+	p = "KacVOUURUgVdDzzquNRitKSnSPjzsDJmeEqKr"
+	kY = 21744316.640195593
+	hBP = False
+	HF = 74305523
+	BUw = "dHvyjxuWaBRUjGwPdOdIvemvLYcaORfXKgCJdSdxvGqxVgyCDMopOnTYHwMoRJGuAahzsjdTYwmifGA"
+	XsJ = 15187667
+	Mep = "HsseIjmwOLLuvLenylWjikdylmfjdYziVzpWaCWZqgnArqPEOTyyqjtk"
+	for NFX in range(135):
+		Tbsl = 57
+	while(True):
+		import time;time.sleep(3)
+		break
+	while(True):
+		import time;time.sleep(3)
+		break
+	for Frm in range(3):
+		lHFYFLhyxB = 118
+	while(True):
+		import time;time.sleep(3)
+		break
+	while(True):
+		import time;time.sleep(3)
+		break
+def lGAJljDx (Ikf):
+	yh = True
+	diW = 14771447
+	Clq = "LPP"
+	DzG = 65223187
+	K = "BUhSroCqDxlZZpgQgjvsATWzJLOfjlBCqaHrYGCvPmMyrtZdvNLVJuiYgVAynTgvlxPnvoVzBHQNCxgpovvuY"
+	for CCOCT in range(124):
+		sckPmrn = 33
+	while(True):
+		import time;time.sleep(3)
+		break
+	for ks in range(117):
+		bv = 140
+	while(True):
+		import time;time.sleep(3)
+		break
+	while(True):
+		import time;time.sleep(3)
+		break
+	for DdPUJ in range(67):
+		YrMbtSZXgy = 69
+	while(True):
+		import time;time.sleep(3)
+		break
+	while(True):
+		import time;time.sleep(3)
+		break
+	for UG in range(20):
+		yXCDVjlBd = 78
+	while(True):
+		import time;time.sleep(3)
+		break
+	for lChjH in range(143):
+		jIdKtFjaCq = 139
+def QWBvr (jWFTDp):
+	UJP = False
+	AB = 69105540
+	for Durxf in range(118):
+		LTpbLOo = 47
+	for bEdp in range(31):
+		OwsJgL = 142
+	while(True):
+		import time;time.sleep(3)
+		break
+	for OE in range(142):
+		WAEwfpS = 79
+def rcPoiSriXt (RHr):
+	YT = True
+	Eg = 2631237
+	tQ = "KjlWcXIBHWoVuBgboMThssKikBWovAHQOhdcqEDeFrKdUCdbFCiUNacThERaSlKgCgkAUYNlCeegvv"
+	RQ = 76683938
+	jeg = "AyLSKeCLjTLgcfklIUDdObuOWkSqGgxReCovVObOzBqkQviJXmdSeOjtHebuayvZzzVvEyQEhMvWSDxpSxZ"
+	Iu = True
+	Qkq = 62221145
+	uo = "AeqfJEkyGnTlHleaKNntahqmVLBISqNIbn"
+	Om = 41098380
+	Zm = "NHBhZLsZiaYeksvQSAfsckwBTRVchKuGnJWasbIAiD"
+	gSq = 66065293.51989623
+	for gD in range(69):
+		EN = 37
